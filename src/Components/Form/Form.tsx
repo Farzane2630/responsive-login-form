b02ef6dc11781c1b2cb94bd3bda80565cd1fc041
Farzaneh <farzanekazemi8517@gmail.com>
2024-07-23 17:47:15 +0330

    fix: deploy bug

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,7 +1,10 @@
-import { useForm } from "react-hook-form"
+import React from "react";
+// import { useForm } from "react-hook-form"
 
 
 export default function Form() {
+   console.log(React.version );
+   
 
    // const {register, handleSubmit} = useForm({})
    return (
